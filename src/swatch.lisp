320f2203439691088c688e80d140e2b900f2e346
tpapp <tkpapp@gmail.com>
2013-01-17 16:32:33 +0100

    Fixed swatch alignment, added newline for multiple swatches.

--- src/swatch.lisp
+++ src/swatch.lisp
@@ -5,20 +5,20 @@
 (defgeneric write-swatch (command style)
   (:documentation "Write a command that produces swatch demonstrating that style."))
 
-(defparameter *swatch-length* (em 2))
-
 (defmethod write-swatch ((command string) (style stroke-style))
-  (latex-newcommand command (1 "1em")
+  (latex-newcommand command (1 "2em")
     (latex-command "flexpicture"
                    (latex-format "#1")
                    (latex-format "2ex")
                    (progn
                      (pgf-set-stroke-style style)
                      (pgf-lines (vector +origin+
-                                        (point *swatch-length* 0)))))))
+                                        (point 1 0)))
+                     (latex-command "pgfsetbaseline" "-0.5ex")))))
 
 (defun write-swatches (filespec command-style-alist &key (if-exists :supersede))
   "Write an alist of COMMAN-STYLE pairs to FILESPEC."
   (with-latex-output (filespec :if-exists if-exists)
     (loop for (command . style) in command-style-alist
-          do (write-swatch command style))))
+          do (write-swatch command style)
+             (latex-newline))))
